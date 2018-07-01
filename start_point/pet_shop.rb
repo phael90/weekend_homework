993f76754455f7b52fedb9072e20d0ad82046cd4
Raphael Ugha <raphaelugha@gmail.com>
2018-07-01 16:57:38 +0100

    passed test_add_or_remove_cash

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -2,3 +2,14 @@ def pet_shop_name(pet_shop)
   name = pet_shop[:name]
   return name
 end
+
+def total_cash(pet_shop)
+  total_cash = pet_shop[:admin][:total_cash]
+  return total_cash
+end
+
+def add_or_remove_cash(pet_shop,add_amount)
+   total_cash = total_cash(pet_shop)
+   total_cash = total_cash + add_amount
+   return pet_shop[:admin][:total_cash] = total_cash
+end
