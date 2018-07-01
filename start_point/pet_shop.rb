0e516fad12288ee3bb3a0135311b3542cd65a7d7
Raphael Ugha <raphaelugha@gmail.com>
2018-07-01 17:19:01 +0100

    passed test_stock_count

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -13,3 +13,19 @@ def add_or_remove_cash(pet_shop,add_amount)
    total_cash = total_cash + add_amount
    return pet_shop[:admin][:total_cash] = total_cash
 end
+
+def pets_sold(pet_shop)
+  pet_sold = pet_shop[:admin][:pets_sold]
+  return pet_sold
+end
+
+
+def increase_pets_sold(pet_shop, add_amount)
+  pet_sold = pet_shop[:admin][:pets_sold]
+  pet_sold += add_amount
+  return pet_shop[:admin][:pets_sold] = pet_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
